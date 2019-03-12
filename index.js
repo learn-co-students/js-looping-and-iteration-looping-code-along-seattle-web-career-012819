3b720a52f932d0eedd54835fcf987d5aa33dba46
Emily Wagner <emilywagner598@gmail.com>
2019-03-12 14:46:40 -0700

    Done

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+function printBadges(arr) {
+    for(let i = 0; i < arr.length; i++) {
+        console.log("Welcome " + (arr[i]) + "! You are employee #" + (i + 1) + ".");
+    }
+    return arr;
+}
+
+function tailsNeverFails() {
+    let cFlip = Math.random();
+    let count = 0;
+
+    while (cFlip >= 0.5 ) {
+        count++;
+        cFlip = Math.random();
+    }
+
+    return ("You got " + count + " tails in a row!");
+}
