2e73150a143dc29f1cdfedbdb603abe858203472
github username <crishanks12@gmail.com>
2019-03-12 17:34:57 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function printBadges(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    const currentSpy = arr[i];
+    console.log(`Welcome ${currentSpy}! You are employee #${i + 1}.`);
+  }
+  return arr;
+}
+
+function tailsNeverFails() {
+  let count = 0;
+
+  while (Math.random() >= 0.5) {
+    count++;
+  }
+  return `You got ${count} tails in a row!`;
+}
